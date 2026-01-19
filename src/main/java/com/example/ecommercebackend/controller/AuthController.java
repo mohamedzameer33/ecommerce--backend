905f2f38adbe435d82a8e988402faa210cf150f8
Mohamed Zameer S M <132653628+mohamedzameer33@users.noreply.github.com>
2026-01-19 17:58:31 +0530

    Update CORS origin for AuthController

--- src/main/java/com/example/ecommercebackend/controller/AuthController.java
+++ src/main/java/com/example/ecommercebackend/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://zam-ecomm.vercel.app")
 public class AuthController {
 
     private final UserService userService;
