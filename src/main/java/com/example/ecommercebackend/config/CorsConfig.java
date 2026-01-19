3750f005d922ba539154f55bc5bb83fdb3c8729f
Mohamed Zameer S M <132653628+mohamedzameer33@users.noreply.github.com>
2026-01-19 17:59:44 +0530

    Update CorsConfig.java

--- src/main/java/com/example/ecommercebackend/config/CorsConfig.java
+++ src/main/java/com/example/ecommercebackend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow all endpoints
-                .allowedOrigins("http://localhost:5173")  // Vite port
+                .allowedOrigins("https://zam-ecomm.vercel.app")  // Vite port
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
