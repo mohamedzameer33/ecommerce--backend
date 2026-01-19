c561d6f5a4992a403f313e11b0af70c36f612dd1
Mohamed Zameer S M <132653628+mohamedzameer33@users.noreply.github.com>
2026-01-19 17:59:10 +0530

    Update CORS origin for ProductController

--- src/main/java/com/example/ecommercebackend/controller/ProductController.java
+++ src/main/java/com/example/ecommercebackend/controller/ProductController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://zam-ecomm.vercel.app")
 public class ProductController {
 
     @Autowired
